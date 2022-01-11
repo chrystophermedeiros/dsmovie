1e2931423d40a1874af29b4225589f564e2429a4
chrystophermedeiros <chrystopher312@gmail.com>
2022-01-11 19:43:53 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar () {
               <a href="https://github.com/chrystophermedeiros" target="_blank" rel="noreferrer">
                   <div className="dsmovie-contact-container">
                       <GithubIcon />
-                      <p className="dsmovie-contact-link">/devsuperior</p>
+                      <p className="dsmovie-contact-link">/chrystophermedeiros</p>
                   </div>
               </a>
           </div>
