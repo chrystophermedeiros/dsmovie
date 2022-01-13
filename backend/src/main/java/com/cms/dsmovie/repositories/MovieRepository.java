4d11eca776c337262ea210319d470f1b267b6250
chrystophermedeiros <chrystopher312@gmail.com>
2022-01-12 23:07:00 -0300

    upadte

---
+++ backend/src/main/java/com/cms/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.cms.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cms.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
